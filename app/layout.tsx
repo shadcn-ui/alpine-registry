5fdeebe06ee4eaa6258c9e9362d8d7a1d1f0a9fc
SameerJS6 <singh6.sameer@gmail.com>
2025-09-02 21:52:22 +0530

    feat: add Geist Mono font and PageWideScrollMask component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next"
-import { DM_Sans } from "next/font/google"
+import { DM_Sans, Geist_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/react"
 import "./globals.css"
 import { Providers } from "@/components/providers"
@@ -9,9 +9,15 @@ import Link from "next/link"
 import { Logo } from "@/registry/alpine/branding/logo"
 import { Toaster } from "@/registry/alpine/ui/sonner"
 import { RegistrySetup } from "@/components/registry-setup"
+import PageWideScrollMask from "@/components/page-wide-adaptive-mask"
 
 const fontSans = DM_Sans({
   variable: "--font-sans",
+  subsets: ["latin"],
+})
+
+const fontGeistMono = Geist_Mono({
+  variable: "--font-geist-mono",
   subsets: ["latin"],
 })
 
@@ -54,16 +60,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${fontSans.variable} font-sans antialiased`}>
+      <body
+        className={`${fontSans.variable} ${fontGeistMono.variable} font-sans antialiased`}
+      >
         <Providers>
           <header>
-            <div className="max-w-7xl mx-auto flex items-center px-4 py-6">
+            <div className="mx-auto flex max-w-7xl items-center px-4 py-6">
               <div className="flex items-center gap-4">
                 <Link href="/">
                   <Logo /> <span className="sr-only">Alpine</span>
                 </Link>
                 <Separator orientation="vertical" className="!h-6" />
-                <p className="text-muted-foreground hidden md:block line-clamp-1 text-sm">
+                <p className="text-muted-foreground line-clamp-1 hidden text-sm md:block">
                   An example registry for distributing code using shadcn.
                 </p>
               </div>
@@ -77,6 +85,7 @@ export default function RootLayout({
           <Toaster position="top-center" />
         </Providers>
         <Analytics />
+        <PageWideScrollMask />
       </body>
     </html>
   )
